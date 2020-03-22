7ddccf10151087ab76459014737c55bfa5fc158a
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-22 14:29:35 -0700

    Add basic functionalities to Category

--- ToDoList.Solution/ToDoList/Controllers/CategoriesController.cs
+++ ToDoList.Solution/ToDoList/Controllers/CategoriesController.cs
@@ -20,5 +20,53 @@ namespace ToDoList.Controllers
       List<Category> model = _db.Categories.ToList(); // ToList() will give us access to all our Categories in List
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Category category)
+    {
+      _db.Categories.Add(category);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Category category)
+    {
+      _db.Entry(category).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      _db.Categories.Remove(thisCategory);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
--- ToDoList.Solution/ToDoList/Startup.cs
+++ ToDoList.Solution/ToDoList/Startup.cs
@@ -30,6 +30,8 @@ namespace ToDoList
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseStaticFiles();
+      
       app.UseDeveloperExceptionPage();
 
       app.UseMvc(routes =>
