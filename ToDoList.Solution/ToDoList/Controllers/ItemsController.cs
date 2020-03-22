b8f9390a2660923a245c9b852d04a12e5b6ac25b
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-22 15:23:30 -0700

    Add CREATE functionaly to ItemsController

--- ToDoList.Solution/ToDoList/Controllers/ItemsController.cs
+++ ToDoList.Solution/ToDoList/Controllers/ItemsController.cs
@@ -21,6 +21,16 @@ namespace ToDoList.Controllers
       return View(_db.Items.ToList());
     }
 
+    // One-to-many relationship code:
+    //     public ActionResult Index()
+    //     {
+    //       List<Item> model = _db.Items.Include(items => items.Category).ToList();
+    //       return View(model);
+    //     }
+    //     // Using eager loading here, meaning that all info related to an object should be loaded by using Include()
+    //     // This basically states: for each Item in the database, include the Category it belongs to and then put all the Items into list
+    //     // We use eager loading instead of lazy loading because each item has only one category so there is a minimal amount of additional loading happening when we get information about an item
+
     public ActionResult Details(int id)
     {
       var thisItem = _db.Items
@@ -30,6 +40,7 @@ namespace ToDoList.Controllers
       return View(thisItem);
     }
     
+    // One-to-many relationship code:
     // public ActionResult Details(int id)
     // {
     //   Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
@@ -37,12 +48,26 @@ namespace ToDoList.Controllers
     //   return View(thisItem);
     // }
 
-    // public ActionResult Create()
-    // {
-    //   ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-    //   return View();
-    // }
+    public ActionResult Create()
+    {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View();
+    }
 
+    [HttpPost]
+    public ActionResult Create(Item item, int CategoryId)
+    {
+      _db.Items.Add(item);
+      if (CategoryId != 0) // we add this condition to handle cases where a CategoryId doesn't get passed in to the route (such as when there are no Categories)
+      {
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+        // this creates the association between the newly created Item and a Category. Because the Item has been added and a new ItemId has been assigned, we can create a new CategoryItem join entity. This combines the ItemId with the CategoryId specified in the dropdown menu and passed in through our route's parameters.
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    // One-to-many relationship code:
     // [HttpPost]
     // public ActionResult Create(Item item)
     // {
